4347878d81a040229c0946aa7b10279d1ce3f6e7
mjwebb1993 <mjwebb1993@yahoo.com>
2021-11-16 10:36:07 -0600

    Section 02 Fix

--- Section02/2.3.3-ComparisonOperators.js
+++ Section02/2.3.3-ComparisonOperators.js
@@ -1,31 +1,33 @@
-// ? log only the expressions that evaluate to true
-// console.log(14 > 8);
-// console.log(42 > 44);
-// console.log(1 > 1);
+//  log only the expressions that evaluate to true
+console.log(14 > 8);
+console.log(42 > 44);
+console.log(1 > 1);
 
-// console.log(31 < 22);
-// 9 < 13;
-// console.log(0 <= 0);
+// this section does something else
+console.log(31 < 22);
+9 < 13;
+// this console log does 1
+console.log(0 <= 0);
 
-// console.log(12 >= 12.5);
-// 60 >= 40;
-// 2 >= 2;
+console.log(12 >= 12.5);
+60 >= 40;
+2 >= 2;
 
-// 12 <= 12.5;
-// 14 <= 0.3;
-// 5 <= 5;
+12 <= 12.5;
+14 <= 0.3;
+5 <= 5;
 
-// console.log(3 === 3);
-// console.log(3 === "3");
-// console.log("3" === "3");
-// console.log(3 === 4);
+console.log(3 === 3);
+console.log(3 === "3");
+console.log("3" === "3");
+console.log(3 === 4);
 
-// console.log(2 !== "2");
-// console.log("2" !== "2");
-// console.log(2 !== 1);
+console.log(2 !== "2");
+console.log("2" !== "2");
+console.log(2 !== 1);
 
-// console.log("two" == 2);
-// console.log(2 == "2");
+console.log("two" == 2);
+console.log(2 == "2");
 2 == 2;
 
 3 != "three";
