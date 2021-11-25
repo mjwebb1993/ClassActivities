c7f0602cbec293996da1fbccd51d476fa642957b
mjwebb1993 <mjwebb1993@yahoo.com>
2021-11-24 18:03:47 -0600

    week 3

--- Section03/3.3.1-Activity/index.js
+++ Section03/3.3.1-Activity/index.js
@@ -1,1 +1,20 @@
-console.log("This message comes from index.js");
+// console.log("Hello World");
+
+function davidInternet(check) {
+  let davidInternetCheck;
+  davidInternetCheck = check;
+
+  if (davidInternetCheck === "Bad") {
+    console.log(
+      "David's internet is more back in the past than Rural Missouri :)"
+    );
+  } else if (davidInternetCheck !== "Bad") {
+    console.log(
+      `"A miracle has occurred this November eve, because instead of being bad, David's internet has been described as: ${davidInternetCheck}`
+    );
+  }
+}
+
+davidInternet(
+  "This is a good example of a template literal interacting within a function btw :) "
+);
