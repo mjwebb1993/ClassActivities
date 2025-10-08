8dbfe8b82f9ce8e143e4a3fd3e10349fff26ef58
Michael Webb <mjwebb1993@yahoo.com>
2025-10-08 14:50:39 -0500

    Up to Primitive Data Types II

---
+++ index.js
@@ -0,0 +1,18 @@
+let str = "Hello, that's Michael 1213 false";
+
+let num = 11.5;
+num = "eleven and a half";
+
+let division = num / 2;
+console.log(division);
+let boo = true;
+
+typeof str;
+console.log(
+  "The variable str has a value of " + str + " and has a typeof " + typeof str
+);
+console.log(typeof num);
+console.log(typeof boo);
+
+let sum = 10 + 5 + 20;
+console.log(sum);
