1d9ae0ed1461384c5fa50b98a288b74bd06098d5
theokyle <theokyle@gmail.com>
2025-10-09 07:06:59 -0500

    completed 2.3.1 through 2.3.3

---
+++ Week2/2.2/2.2.2-JSMorePrimitives.js
@@ -0,0 +1,23 @@
+// create five variables and assign them values
+// each variable should reference a different primitive data type
+// REMEMBER: you should never DELIBERATELY assign "undefined" to a variable
+let age = 50;
+let career = false;
+let codingClass = "bootcamp";
+let nothing = null;
+let undefined;
+
+// print the type of each variable in this order:
+// string, number, boolean, undefined, null
+console.log(typeof "bootcamp")
+
+// create a variable that references a template literal
+// inside the template literal, use two of the above variables
+
+// reassign the value of the variable that references "null"
+
+// print the value and its type
+
+// print a variable that causes a ReferenceError
+
+// alter the previous line to no longer cause a ReferenceError
